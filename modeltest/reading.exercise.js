1d913921512f38e30be3287f5a046350dfdeb0a0
Ignacio Suay <ignacio.suay@gmail.com>
2015-04-20 17:13:49 +0100

    Add reading ex

---
+++ modeltest/reading.exercise.js
@@ -0,0 +1,48 @@
+[{
+    "title": "",
+    "content":  "&lt;p> We live on the island of Hale. It's about four kilometres long and two kilometres wide at its broadest point, and it's joined to the mainland by a causeway called the Stand - a narrow road built across the mouth of the river which separates us from the rest of the country. Most of the time you wouldn't know we're on an island because the river mouth between us and the mainland is just a vast stretch of tall grasses and brown mud. But when there's a high tide and the water rises a half a metre or so above the road and nothing can pass until the tide goes out again a few hours later, then you know it's an island." +
+                "&lt;p> We were on our way back from the mainland. My older brother, Dominic, had just finished his first year at university in a town 150 km away. Dominic's train was due in at five and he'd asked for a lift back from the station. Now, Dad normally hates being disturbed when he's writing (which is just about all the time), and he also hates having to go anywhere, but despite the typical sighs and moans – why can't he get a taxi? what's wrong with the bus? – I could tell by the sparkle in his eyes that he was really looking forward to seeing Dominic. "+
+                "&lt;p> So, anyway, Dad and I had driven to the mainland and picked up Dominic from the station. He had been talking non-stop from the moment he'd slung his rucksack in the boot and got in the car. University this, university that, writers, books, parties, people, money, gigs.... And when I say talking, I don't mean talking as in having a conversation, I mean talking as in jabbering like a mad thing. I didn't like it .... the way he spoke and waved his hands around as if he was some kind of intellectual or something. It was embarrassing. It made me feel uncomfortable – that kind of discomfort you feel when someone you like, someone close to you, suddenly starts acting like a complete idiot. And I didn't like the way he was ignoring me, either. For all the attention I was getting I might as well not have been there. I felt a stranger in my own car." +
+                "&lt;p> As we approached the island on that Friday afternoon, the tide was low and the Stand welcomed us home, stretched out before us, clear and dry, beautifully hazy in the heat – a raised strip of grey concrete bound by white railings and a low footpath on either side, with rough cobbled banks leading down to the water. Beyond the railings, the water was glinting with that wonderful silver light we sometimes get here in the late afternoon which lazes through to the early evening." +
+                "&lt;p> We were about halfway across when I saw the boy. My first thought was how odd it was to see someone walking on the Stand. You don't often see people walking around here. Between Hale and Moulton (the nearest town about thirty kilometres away on the mainland), there's nothing but small cottages, farmland, heathland and a couple of hills. So islanders don't walk <strong>because of that</strong>. If they're going to Moulton they tend to take the bus. So the only pedestrians you're likely to see around here are walkers or bird-watchers. But even from a distance I could tell that the figure ahead didn't fit into either of these categories. I wasn't sure how I knew, I just did." +
+                "&lt;p> As we drew closer, he became clearer. He was actually a young man rather than a boy. Although he was on the small side, he wasn't as slight as I'd first thought. He wasn't exactly muscular, but he wasn't weedy-looking either. It's hard to explain. There was a sense of strength about him, a graceful strength that showed in his balance, the way he held himself, the way he walked....",
+    "exerciseType": "READING_1",
+    "responses": [
+        {
+            "_id": 31,
+            "statement":"In the first paragraph, what is Caitlin's main point about the island?",
+            "answers": ["It can be dangerous to try to cross from the mainland.", "It is much smaller than it looks from the mainland.", "It is only completely cut off at certain times.", "It can be a difficult place for people to live in."],
+            "correct": 2,
+            "responseType": "VOCABULARY"},
+        {
+            "_id": 32,
+            "statement":"What does Caitlin suggest about her father?",
+            "answers": ["His writing prevents him from doing things he wants to with his family.", "His initial reaction to his son's request is different from usual.", "His true feelings are easily hidden from his daughter.", "His son's arrival is one event he will take time off for."],
+            "correct": 3,
+            "responseType": "VOCABULARY"},
+        {
+            "_id": 33,
+            "statement":"Caitlin emphasises her feelings of discomfort because she",
+            "answers": ["is embarrassed that she doesn't understand what her brother is talking about.", "feels confused about why she can't relate to her brother any more.", "is upset by the unexpected change in her brother's behaviour.", "feels foolish that her brother's attention is so important to her."],
+            "correct": 2,
+            "responseType": "VOCABULARY"},
+        {
+            "_id": 34,
+            "statement":"In the fourth paragraph, what is Caitlin's purpose in describing the island?",
+            "answers": ["to express her positive feelings about it","to explain how the road was built", "to illustrate what kind of weather was usual", "to describe her journey home"],
+            "correct": 0,
+            "responseType": "VOCABULARY"},
+        {
+            "_id": 35,
+            "statement":"In 'because of that' the word 'that' refers to",
+            "answers": ["locals thinking it is odd to walk anywhere.","it being easier for people to take the bus than walk.","people having everything they need on the island.", "there being nowhere in particular to walk to from the island."],
+            "correct": 3,
+            "responseType": "VOCABULARY"},
+        {
+            "_id": 36,
+            "statement":"What do we learn about Caitlin's reactions to the boy?",
+            "answers": ["She felt his air of confidence contrasted with his physical appearance.", "She was able to come up with a reason for him being there.", "She realised her first impression of him was inaccurate.", "She thought she had seen him somewhere before."],
+            "correct": 2,
+            "responseType": "VOCABULARY"},
+    ]
+    }]
