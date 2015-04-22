202ffe2a9c8a1e477f00d73f2f39f13ebd400eb9
ignacio.suay <ignacio.suay@gmail.com>
2015-04-22 18:48:50 +0100

    Add reading View

--- src/main/webapp/scripts/app/exercise/reading/ReadingController.js
+++ src/main/webapp/scripts/app/exercise/reading/ReadingController.js
@@ -3,5 +3,14 @@
 angular.module('firstcertificatetestsApp')
     .controller('ReadingController', function ($scope,$state) {
 
+        $scope.loadAll = function() {
+            Exercise.exam.query({examName:"fce1", exerciseType:"READING"}, function(result) {
+                $scope.exercises= result;
+                $scope.exercise = $scope.exercises[0];
+            });
+        };
+
+        $scope.loadAll();
+
 
     });
