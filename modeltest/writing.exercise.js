935e4c31c8977d34a4f0d480228c58fb4c2b24e6
ignacio.suay <ignacio.suay@gmail.com>
2015-05-01 09:28:40 +0100

    add writing exercise json

---
+++ modeltest/writing.exercise.js
@@ -0,0 +1,30 @@
+db.exercise.insert(
+    {
+        "title": "",
+        "content":  "Every country in the world has problems with pollution and damage to the environment. Do you think these problems can be solved? <br/> Notes <br/> Write about <br/> 1. transport <br/> 2. rivers and seas <br/> 3...",
+        "exerciseType": "WRITING_1",
+        "examName": "fce1"
+    });
+
+db.exercise.insert(
+    {
+        "title": "",
+        "content":  "You see this announcement in your college English-language magazine.<div class='well'> <p>Book reviews wanted</p><p>Have you read a book in which the main character behaved in a surprising way?</p><p>Write us a review of the book, explaining what the main character did and why it was surprising. Tell us whether or not you would recommend this book to other people.</p><p>The best reviews will be published in the magazine.</p> </div> Write your review. ",
+        "exerciseType": "WRITING_2",
+        "examName": "fce1"
+    });
+
+db.exercise.insert(
+    {
+        "title": "",
+        "content":  "You see this announcement on an English-language website. <div class='well'><p>Articles wanted</p><p>The most useful thing I have ever learned.</p><p>What is the most useful thing you have learned?Who did you learn it from? Why is it useful?<p>Write us an article answering these questions.</p><p>We will publish the best articles on our website.</p> <div class='well'>Write your article.</div> ",
+        "exerciseType": "WRITING_3",
+        "examName": "fce1"
+    });
+db.exercise.insert(
+    {
+        "title": "",
+        "content":  "You have received this email from your English-speaking friend David. <div class='well'><p>From: David <br/> Subject: touring holiday</p><p>Some college friends of mine are visiting your area soon for a week’s touring holiday. They would like to travel around and learn about your local area and its history.</p><p>Can you tell me about some of the places they could visit? What’s the best way to travel around – car, bike or coach?</p><p>Thanks</p><p>David</p> <div class='well'>Write your email</div> ",
+        "exerciseType": "WRITING_4",
+        "examName": "fce1"
+    });
