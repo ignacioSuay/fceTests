f10a5b327da23d7a0d03b1aef2ab3f059f94907a
Ignacio Suay <ignacio.suay@gmail.com>
2015-05-12 16:48:42 +0100

    Show alert message if you are not logged in

--- src/main/webapp/scripts/app/exercise/ExerciseController.js
+++ src/main/webapp/scripts/app/exercise/ExerciseController.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('firstcertificatetestsApp')
-    .controller('ExerciseController', function ($scope,$state, Exercise) {
+    .controller('ExerciseController', function ($scope,$state, Exercise, Principal) {
 
         //change nav active when click
         $(".nav a").on("click", function(){
             $(".nav").find(".active").removeClass("active");
             $(this).parent().addClass("active");
+        });
+        Principal.identity().then(function(account) {
+            $scope.account = account;
+            $scope.isAuthenticated = Principal.isAuthenticated;
         });
 
     });
