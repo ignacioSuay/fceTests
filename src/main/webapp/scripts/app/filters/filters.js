237b3ecd8f81a604e59716516c9725eff4b3b41b
ignacio.suay <ignacio.suay@gmail.com>
2015-05-17 21:40:34 +0100

    Create a filter to show the exercise type in a proper form

--- src/main/webapp/scripts/app/filters/filters.js
+++ src/main/webapp/scripts/app/filters/filters.js
@@ -20,4 +20,25 @@ angular.module('firstcertificatetestsApp')
                 return minsStr + ':' + secsStr;
             }
         };
+    })
+    .filter('exerciseTypeFilter', function () {
+        return function (exerciseType) {
+            if(exerciseType){
+                if(exerciseType === "USE_OF_ENGLISH_1"){return "Use of English - Part 1"}
+                if(exerciseType === "USE_OF_ENGLISH_2"){return "Use of English - Part 2"}
+                if(exerciseType === "USE_OF_ENGLISH_3"){return "Use of English - Part 3"}
+                if(exerciseType === "USE_OF_ENGLISH_4"){return "Use of English - Part 4"}
+                if(exerciseType === "READING_1"){return "Reading - Part 1"}
+                if(exerciseType === "READING_2"){return "Reading - Part 2"}
+                if(exerciseType === "READING_3"){return "Reading - Part 3"}
+                if(exerciseType === "WRITING_1"){return "Writing - Part 1"}
+                if(exerciseType === "WRITING_2"){return "Writing - Part 2"}
+                if(exerciseType === "WRITING_3"){return "Writing - Part 3"}
+                if(exerciseType === "WRITING_4"){return "Writing - Part 4"}
+                if(exerciseType === "SPEAKING_1"){return "Speaking - Part 1"}
+                if(exerciseType === "SPEAKING_2"){return "Speaking - Part 2"}
+                if(exerciseType === "SPEAKING_3"){return "Speaking - Part 3"}
+                if(exerciseType === "SPEAKING_4"){return "Speaking - Part 4"}
+            }
+        }
     });
