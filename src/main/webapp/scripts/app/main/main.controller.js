9685de76421955ebcdcaa0101b86b4cb48431ed5
Ignacio Suay <ignacio.suay@gmail.com>
2015-07-23 17:21:09 +0100

    add contact me form 1st step

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -6,4 +6,14 @@ angular.module('firstcertificatetestsApp')
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
         });
+
+        $(function() {
+            $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+                $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+            }).on("focus", ".floating-label-form-group", function() {
+                $(this).addClass("floating-label-form-group-with-focus");
+            }).on("blur", ".floating-label-form-group", function() {
+                $(this).removeClass("floating-label-form-group-with-focus");
+            });
+        });
     });
