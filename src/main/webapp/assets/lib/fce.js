656f19978781a90ed591e64a474fa93ca43953f6
Ignacio Suay <ignacio.suay@gmail.com>
2015-10-01 16:35:13 +0100

    fix navbar style issue

--- src/main/webapp/assets/lib/fce.js
+++ src/main/webapp/assets/lib/fce.js
@@ -17,10 +17,10 @@
   });
 
   // Highlight the top nav as scrolling occurs
-  $('body').scrollspy({
-    target: '.navbar-fixed-top',
-    offset: 55
-  });
+  //$('body').scrollspy({
+  //  target: '.navbar-fixed-top',
+  //  offset: 55
+  //});
 
   // Closes the Responsive Menu on Menu Item Click
   $('.navbar-collapse ul li a').click(function() {
